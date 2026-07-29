bee37779426d5f2708abe9bba60bdb4c60d6c635
jungeunkim <grace0130kim@gmail.com>
2026-07-29 22:01:53 +0900

    Improve About section layout

--- components/About.tsx
+++ components/About.tsx
@@ -26,7 +26,7 @@ export default function About() {
       </h2>
 
 
-      <div className="grid md:grid-cols-3 gap-6">
+      <div className="grid md:grid-cols-2 gap-6">
         {/* Profile */}
         <Card title="Profile">
             <p>
